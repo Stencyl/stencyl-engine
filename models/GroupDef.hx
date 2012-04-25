bc6b6cdca568d38da2d46392929c5248399a503d
Jon <jon@stencyl.com>
2012-04-25 16:51:38 +0000

    2.5 work

---
+++ models/GroupDef.hx
@@ -0,0 +1,13 @@
+package models;
+
+public class GroupDef
+{
+	public var ID:Int;
+	public var name:String;
+	
+	public function new(ID:Int, name:String)
+	{
+		this.ID = ID;
+		this.name = name;
+	}
+}
