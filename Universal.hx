9c386e7dbbda6fa974e80737cba507f59125bf12
Jon <jon@stencyl.com>
2012-07-27 00:33:10 +0000

    [3.0] visual studio

--- Universal.hx
+++ Universal.hx
@@ -52,7 +52,7 @@ class Universal extends Sprite
 		
 		//Tablets and other high-res devices get to use 2x mode, (TODO: if it's not a tablet-only game.)
 		#if (mobile)		
-		if(stage.stageWidth >= 960 || stage.stageHeight >= 960)
+		if(stage.stageWidth >= 800 || stage.stageHeight >= 800)
 		{
 			Engine.SCALE = 2;
 			Engine.IMG_BASE = "2x";
@@ -60,8 +60,8 @@ class Universal extends Sprite
 		
 		else
 		{
-			Engine.SCALE = 2;
-			Engine.IMG_BASE = "2x";
+			Engine.SCALE = 1;
+			Engine.IMG_BASE = "1x";
 		}
 		#end
 		
