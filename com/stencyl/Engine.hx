ca957178f0e8eaf405a92d77b26f384f912bd682
Mike <legendarymmsx@hotmail.com>
2013-04-29 06:42:26 +0000

    Revert test changes

--- com/stencyl/Engine.hx
+++ com/stencyl/Engine.hx
@@ -632,8 +632,6 @@ class Engine
 		
 		#if cpp
 		Gc.run(true);
-		trace("Number of IntHashs: " + Gc.trace(IntHash, false));
-		trace("Number of Actors: " + Gc.trace(Actor, false));
 		#end
 		
 		for(atlas in atlasesToLoad)
