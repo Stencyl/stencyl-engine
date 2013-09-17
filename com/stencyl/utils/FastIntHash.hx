39e80e97d89d1ece6b6a66226be43daed10b0b18
Mike <legendarymmsx@hotmail.com>
2013-09-17 07:07:35 +0000

    [3.0] Add/Setup new FastIntHash class with new iterator.  Not implemented in engine yet.

---
+++ com/stencyl/utils/FastIntHash.hx
@@ -0,0 +1,29 @@
+package com.stencyl.utils;
+
+class FastIntHash<T> extends IntHash<T>
+{	
+	public var obj:Dynamic;
+	
+	public function new() { super(); obj = h; }
+	
+	override public function iterator():Iterator<T> 
+	{
+		#if (cpp)		
+		trace("THIS IS WHAT it is doing");
+		untyped return __global__.__int_hash_fast_values(h);
+		#else
+		return super.iterator();
+		#end
+		
+	}	
+	
+	override public function keys():Iterator<Int> 
+	{
+		#if (cpp)		
+		untyped return __global__.__int_hash_fast_keys(h);
+		#else
+		return super.keys();
+		#end
+		
+	}
+}
