e32ddbd30ad2c35fa31449f5a3678224200dbb54
Jon <jon@stencyl.com>
2014-02-28 09:00:17 +0000

    [PUBLIC] [3.0] Initial work on an Image API - Implement get image for actor on C++ targets.

--- com/stencyl/graphics/SheetAnimation.hx
+++ com/stencyl/graphics/SheetAnimation.hx
@@ -240,6 +240,8 @@ class SheetAnimation extends Sprite implements AbstractAnimation
 	
 	public function getCurrentImage():BitmapData
 	{
-		return null; //TODO - Ask rob for details.
+		var img = new BitmapData(Std.int(width), Std.int(height), true, 0x00ffffff);
+		img.copyPixels(tilesheet.__bitmap, new nme.geom.Rectangle(frameIndex * width, 0, Std.int(width), Std.int(height)), new nme.geom.Point(0, 0), null, null, false);
+		return img;
 	}
 }
