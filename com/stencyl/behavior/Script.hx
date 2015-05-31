61b0fb26e3fc3199e71192442a132ceed348f694
captaincomic <darioseidl@fastmail.fm>
2015-05-31 08:43:34 +0000

    Fix: regression: revert serializing of attributes on save/load

--- com/stencyl/behavior/Script.hx
+++ com/stencyl/behavior/Script.hx
@@ -3519,11 +3519,9 @@ class Script
 	{
 		var so = SharedObject.getLocal(fileName);
 		
-		Reflect.setField(so.data, "serialized", true);
-		
 		for(key in engine.gameAttributes.keys())
 		{
-			Reflect.setField(so.data, key, haxe.Serializer.run(engine.gameAttributes.get(key)));
+			Reflect.setField(so.data, key, engine.gameAttributes.get(key));
 		}	
 
 		#if flash
@@ -3574,23 +3572,12 @@ class Script
 		
 		trace("Loaded Save: " + fileName);
 		
-		var serialized:Bool = Reflect.hasField(data.data, "serialized") && Reflect.field(data.data, "serialized") == true;
-
 		for(key in Reflect.fields(data.data))
 		{
-			trace(key + " - " + Reflect.field(data.data, key));
-			
-			if (serialized)
-			{
-				engine.gameAttributes.set(key, haxe.Unserializer.run(Reflect.field(data.data, key)));
-			}
-			
-			else
-			{
-				engine.gameAttributes.set(key, Reflect.field(data.data, key));
-			}
+			trace(key + " - " + Reflect.field(data.data, key));		
+			engine.gameAttributes.set(key, Reflect.field(data.data, key));
 		}
-			
+		
 		onComplete(true);
 	}
 	
