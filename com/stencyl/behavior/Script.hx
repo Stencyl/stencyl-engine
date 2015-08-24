9fbe066c51e6bf6238209a2f92447d9e5927e12f
rob1221 <robert.alvarez86@gmail.com>
2015-08-24 13:05:09 -0400

    Fixed sound length blocks for desktop and iOS.

--- com/stencyl/behavior/Script.hx
+++ com/stencyl/behavior/Script.hx
@@ -2107,11 +2107,7 @@ class Script
 		
 		if(sc != null && sc.currentSource != null)
 		{
-			#if flash
 			return sc.currentSource.length;
-			#else
-			return sc.currentSource.length * 1000;
-			#end
 		}
 		
 		else
@@ -2127,11 +2123,7 @@ class Script
 	{			
 		if(clip != null && clip.src != null)
 		{
-			#if flash
 			return clip.src.length;
-			#else
-			return clip.src.length * 1000;
-			#end
 		}
 		
 		else
