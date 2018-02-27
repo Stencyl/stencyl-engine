7dc5235236e952b2bb609f8857828f3242055899
Robert Alvarez <robert.alvarez86@gmail.com>
2018-02-27 12:27:08 -0500

    The "current screen as image" block now scales as expected when attaching it back to the screen (issue #124).

--- com/stencyl/graphics/BitmapWrapper.hx
+++ com/stencyl/graphics/BitmapWrapper.hx
@@ -34,9 +34,19 @@ class BitmapWrapper extends Sprite implements EngineScaleUpdateListener
 
 		if(autoscale)
 		{
-			var mtx = new Matrix();
-			mtx.scale(Engine.SCALE, Engine.SCALE);
-			transform.matrix = mtx;
+			// The "current screen as image" block is already scaled, so change the bitmap to match a normal image
+			if (img.width == Std.int(Engine.stage.stageWidth) && img.height == Std.int(Engine.stage.stageHeight))
+			{
+				var mtx = new Matrix();
+				mtx.scale(1 / Engine.screenScaleX, 1 / Engine.screenScaleY);
+				transform.matrix = mtx;
+			}
+			else
+			{
+				var mtx = new Matrix();
+				mtx.scale(Engine.SCALE, Engine.SCALE);
+				transform.matrix = mtx;
+			}
 		}
 	}
 
