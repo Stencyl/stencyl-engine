Stencyl/stencyl-engine

commit e789ba7c2273af06adfa9146386cadc85411e7d9

In-layer ordering of actors is preserved from toolset

com/stencyl/Engine.hx
@@ -1325,6 +1325,15 @@ class Engine
 		{
 			actorsToCreate.push(createActor(instance, true));
 		}
+		for(layer in interactiveLayers)
+		{
+			for(i in 0...layer.actorContainer.numChildren)
+			{
+				var actor:Actor = cast layer.actorContainer.getChildAt(i);
+				var actorInstance = scene.actors.get(actor.ID);
+				layer.actorContainer.swapChildrenAt(i, actorInstance.orderInLayer);
+			}
+		}
 	}
 	
 	private function loadDeferredActors()
@@ -2422,6 +2431,7 @@ class Engine
 			1,
 			1,
 			layerID,
+			-1,
 			0,
 			type.groupID,
 			type.ID,

com/stencyl/io/mbs/Typedefs.hx
@@ -112,6 +112,6 @@ class Typedefs extends MbsTypedefSet
 	
 	override public function getHash():Int
 	{
-		return 1466594936;
+		return -2033963926;
 	}
 }

com/stencyl/io/mbs/scene/MbsActorInstance.hx
@@ -22,6 +22,7 @@ class MbsActorInstance extends MbsObject
 	public static var x:MbsField;
 	public static var y:MbsField;
 	public static var z:MbsField;
+	public static var orderInLayer:MbsField;
 	public static var snippets:MbsField;
 	
 	public static var MBS_ACTOR_INSTANCE:ComposedType;
@@ -42,6 +43,7 @@ class MbsActorInstance extends MbsObject
 		x = MBS_ACTOR_INSTANCE.createField("x", INTEGER);
 		y = MBS_ACTOR_INSTANCE.createField("y", INTEGER);
 		z = MBS_ACTOR_INSTANCE.createField("z", INTEGER);
+		orderInLayer = MBS_ACTOR_INSTANCE.createField("orderInLayer", INTEGER);
 		snippets = MBS_ACTOR_INSTANCE.createField("snippets", LIST);
 		
 	}
@@ -177,6 +179,16 @@ class MbsActorInstance extends MbsObject
 	public function setZ(_val:Int):Void
 	{
 		data.writeInt(address + z.address, _val);
+	}
+	
+	public function getOrderInLayer():Int
+	{
+		return data.readInt(address + orderInLayer.address);
+	}
+	
+	public function setOrderInLayer(_val:Int):Void
+	{
+		data.writeInt(address + orderInLayer.address, _val);
 	}
 	
 	public function getSnippets():MbsList<MbsSnippet>

com/stencyl/models/Scene.hx
@@ -693,6 +693,7 @@ class Scene
 		var scaleY = r.getScaleY();
 		
 		var layerID = r.getZ();
+		var orderInLayer = r.getOrderInLayer();
 		var angle = Std.int(r.getAngle());
 		var groupID = r.getGroupID();
 		var actorID = r.getId();
@@ -729,6 +730,7 @@ class Scene
 			scaleX,
 			scaleY,
 			layerID,
+			orderInLayer,
 			angle,
 			groupID,
 			actorID,

com/stencyl/models/scene/ActorInstance.hx
@@ -12,6 +12,7 @@ class ActorInstance
 	public var scaleX:Float;
 	public var scaleY:Float;
 	public var layerID:Int;
+	public var orderInLayer:Int;
 	public var angle:Int;
 	public var groupID:Int;
 	public var actorID:Int;
@@ -28,6 +29,7 @@ class ActorInstance
 		scaleX:Float,
 		scaleY:Float,
 		layerID:Int,
+		orderInLayer:Int,
 		angle:Int,
 		groupID:Int,
 		actorID:Int,
@@ -41,6 +43,7 @@ class ActorInstance
 		this.scaleX = scaleX;
 		this.scaleY = scaleY;
 		this.layerID = layerID;
+		this.orderInLayer = orderInLayer;
 		this.angle = angle;
 		this.groupID = groupID;
 		

commit 078e78ed52522316c2297e4166f623c299b143f5

Don't place hidden actors on layers
Fixes bug caused by e789ba7c2273af06adfa9146386cadc85411e7d9
http://community.stencyl.com/index.php?issue=1500.0

com/stencyl/Engine.hx
@@ -1354,7 +1354,7 @@ class Engine
 	
 	private function loadCamera()
 	{
-		camera = new Actor(this, -1, GameModel.DOODAD_ID, 0, 0, topLayer.ID, 2, 2, null, null, null, null, true, false, true, false, null, true, false);
+		camera = new Actor(this, -1, GameModel.DOODAD_ID, 0, 0, -1, 2, 2, null, null, null, null, true, false, true, false, null, true, false);
 		camera.name = "Camera";
 		camera.isCamera = true;
 		cameraX = 0;
@@ -1529,7 +1529,7 @@ class Engine
 					GameModel.TERRAIN_ID,
 					wireframe.x, 
 					wireframe.y, 
-					topLayer.ID, 
+					-1, 
 					Std.int(p.width), 
 					Std.int(p.height), 
 					null, 
@@ -1555,7 +1555,7 @@ class Engine
 					GameModel.TERRAIN_ID,
 					wireframe.x, 
 					wireframe.y, 
-					topLayer.ID, 
+					-1, 
 					Std.int(wireframe.width), 
 					Std.int(wireframe.height), 
 					null, 