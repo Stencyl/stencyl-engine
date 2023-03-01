ef3da12ec60bffbee921f94183a83d25c009667c
Justin Espedal <justin.espedal@gmail.com>
2023-03-02 02:48:59 +0900

    Move admob to its own extension

--- com/stencyl/APIKeys.hx
+++ com/stencyl/APIKeys.hx
@@ -2,9 +2,5 @@ package com.stencyl;
 
 class APIKeys
 {
-	public static inline var whirlID:String = "";
-	public static inline var whirlID1:String = "";
-	public static inline var ioswhirlID:String = "";
-	public static inline var ioswhirlID1:String = "";
 	public static inline var androidPublicKey:String = "";
 }
