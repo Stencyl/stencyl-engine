fdbc26283461344fd7239abf24adda9907eab55e
Justin Espedal <justin.espedal@gmail.com>
2023-03-14 18:19:45 +0900

    Move Label code to its own extension

--- com/stencyl/graphics/fonts/TextAlign.hx
+++
@@ -1,8 +0,0 @@
-package com.stencyl.graphics.fonts;
-
-class TextAlign 
-{
-	public static inline var LEFT:Int = 1;
-	public static inline var RIGHT:Int = 2;
-	public static inline var CENTER:Int = 3;
-}
